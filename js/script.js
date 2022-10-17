afa8186f973d81edd3a6188143d9675a102b1772
Omar-Codess <omartaki8@gmail.com>
2022-10-17 19:09:52 +0200

    vari test in javascript

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 // Creare l'array con numeri da 1 a 100
 const squaresNumber = 100;
 
-let i = 0
-while (i < squaresNumber){
-    i++
-    console.log(i);
+
+
+
+
+
+
+
+
+
+// Funzioni
+// Per generare numeri ordinati
+const generatedNumbers
+let array = [];
+for (let i = 0; i < array.length; i++) {
+    if (i < 100)
+    const generatedNumbers = array[i];
+    
 }
+console.log(array);
+
+
+
+
+
+
+// TEST
