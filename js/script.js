cebdfcb424c624a55b7fac2c68615f39e464b48f
Omar-Codess <omartaki8@gmail.com>
2022-10-18 12:38:49 +0200

    scritto pseudo codice

--- js/script.js
+++ js/script.js
@@ -1,29 +1,24 @@
-// Creare l'array con numeri da 1 a 100
-const squaresNumber = 100;
+// Generare una griglia quadrata formata da 100 celle, in cui ogni cella contiene un numero da 1 a 100.
+// Quando l'utente clicca su una cella, diventa azzurra.
+
+// ESECUZIONE
+/*
+    Quando l'utente clicca sul bottone:
+    -nascondere il titolo
+    -ripulire la griglia
+    -creare (il numero di quadrati da generare) square:
+        *creare l'elemento
+        *aggiungere la classe
+        *inserire l'elemento
+        *aggiungere il click listener allo square
+            .al click colorare lo square cliccato di azzurro e scrivere in console il numero
+        *appendere lo square alla griglia
+    -mostrare la griglia
 
 
 
 
 
 
+*/
 
-
-
-
-// Funzioni
-// Per generare numeri ordinati
-const generatedNumbers
-let array = [];
-for (let i = 0; i < array.length; i++) {
-    if (i < 100)
-    const generatedNumbers = array[i];
-    
-}
-console.log(array);
-
-
-
-
-
-
-// TEST
