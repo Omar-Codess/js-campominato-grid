d719eb19a45185c6196379375b5905709465dbdb
Omar-Codess <omartaki8@gmail.com>
2022-10-18 17:59:09 +0200

    rifatta la parte di javascript

--- js/script.js
+++ js/script.js
@@ -16,3 +16,41 @@
     -mostrare la griglia
 */
 
+const mainTitle = document.getElementById("main-title");
+console.log(mainTitle);
+const mainGrid = document.getElementById("grid");
+console.log(mainGrid);
+document.getElementById("play-button").addEventListener("click", startGame);
+
+// Funzione principale del gioco
+
+function startGame(){
+    let squaresNumber = 100
+    let squaresInRow = 10
+
+    mainTitle.classList.add("hidden");
+    mainGrid.innerHTML = "";
+    for (let i = 1; i <= squaresNumber; i++) {
+        const newSquare = createSquare(i, squaresInRow);
+        newSquare.addEventListener("click", handleSquareClick);
+        mainGrid.append(newSquare);
+    }
+
+    mainGrid.classList.remove("hidden");
+}
+
+// Funzione che crea le celle
+function createSquare(innerNumber) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `calc(100% / 10)`;
+    square.style.height = `calc(100% / 10)`;
+    square.innerHTML = `<span>${innerNumber}</span>`;
+    return square;
+}
+
+// Funzione che gestisce il click sulla cella
+function handleSquareClick(){
+    console.log(this.textContent);
+    this.classList.add("clicked");
+}
